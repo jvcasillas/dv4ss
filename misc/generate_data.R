5c91529ceb747976116ddc6252aa8c8287224572
Joseph V. Casillas <joseph.casillas@rutgers.edu>
2023-09-06 07:26:02 -0400

    update data again

--- misc/generate_data.R
+++ misc/generate_data.R
@@ -1,9 +1,15 @@
 dat <- mtcars |>
-  select(score = mpg, proficiency = drat, group = am) |>
+  select(score = mpg, proficiency = drat, group = am, age = cyl) |>
   mutate(
     score = (score - min(score)) / (max(score) - min(score)), 
     proficiency = ((proficiency - mean(proficiency)) / sd(proficiency)), 
-    group = if_else(group == 1, "bilingual", "monolingual")
+    group = if_else(group == 1, "bilingual", "monolingual"), 
+    age = case_when(
+      age == 4 ~ "young", 
+      age == 6 ~ "older", 
+      age == 8 ~ "oldest"
+    ), 
+    age = fct_relevel(age, "young", "older")
   ) |>
   mutate_if(is.numeric, round, digits = 2)
 
@@ -15,6 +21,11 @@ ggplot(data = dat) +
 
 ggplot(data = dat) + 
   aes(x = group, y = score) + 
+  geom_point()
+
+ggplot(data = dat) + 
+  aes(x = group, y = score) + 
+  facet_wrap(. ~ age) + 
   geom_point()
 
 saveRDS(dat, file = here("misc", "new_data.RDS"))
